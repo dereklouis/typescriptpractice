36acdf53ec736ea915bf2739e9bdbe7e9d189364
Derek Louis <thedereklouis@gmail.com>
2021-05-20 18:10:23 -0400

    working clock!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface TimeObj {
+  hours: number,
+  minutes: number,
+  seconds: number
+}
+
+declare global {
+  interface Window { myInt: any; }
+}
+
 function App() {
+
+  const [currentTime, setCurrentTime] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  });
+
+  const updateTime = () => {
+    const time: Date = new Date();
+  const adjustedHours = time.getHours() > 12 ? time.getHours() : time.getHours() - 12;
+  let TO: TimeObj = {
+    hours: adjustedHours,
+    minutes: time.getMinutes(),
+    seconds: time.getSeconds()
+  }
+  setCurrentTime(TO);
+  }
+
+  useEffect(() => {
+    updateTime();
+    window.myInt = undefined;
+  }, [])
+
+  const timeInt = () => {
+    if (window.myInt === undefined) {
+      window.myInt = setInterval(() => {
+        updateTime();
+      }, 1000)
+    }
+  }
+
+  timeInt();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="clock">
+        <div id="hourHand" style={{
+        transform: `rotate(${(30 * currentTime.hours) + ((Math.floor(currentTime.minutes / 12)) * 6)}deg)`
+      }}></div>
+      <div id="minuteHand" style={{
+        transform: `rotate(${6 * currentTime.minutes}deg)`
+      }}></div>
+    <div id="secondHand" style={{
+        transform: `rotate(${6 * currentTime.seconds}deg)`
+      }}></div>
+      <div id="nobContainer">
+      <div id="nob"></div>
+      </div>
+      <img src="clockNumbers.png" id="clockNumbers" alt="Clock Numbers" />
+    </div>
     </div>
   );
 }
